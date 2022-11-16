30fab1a7dcc17e278bc62942535f97268b1b6383
likey99 <lkeyue@163.com>
2022-11-16 18:29:49 +0800

    quiz2

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -15,14 +15,15 @@
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new();// TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
+    basket.insert(String::from("apple"), 4);
 
     // TODO: Put more fruits in your basket here.
 
-    basket
+    basket.insert(String::from("mango"), 4);
 }
 
 #[cfg(test)]
--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -36,7 +36,7 @@ mod tests {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
         let your_order = Order {
-            name: "Hacker in Rust".to_sreing(),
+            name: "Hacker in Rust".to_string(),
             count:1,
             ..order_template
 
